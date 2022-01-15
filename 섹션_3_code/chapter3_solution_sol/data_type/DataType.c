19bc1dea1953dd04694b7bb42f54ea9fa71f2abb
lgsc0409ld@gmail.com <lgsc0409ld@gmail.com>
2022-01-16 00:19:53 +0900

    부동 소수점까지

---
+++ 섹션_3_code/chapter3_solution_sol/data_type/DataType.c
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
+/*
+int main()
+{
+	char c = 65;
+	short s = 200;
+	unsigned int ui = 3000000000U;//3,000,000,000U u는 생략해도됨
+	long l = 65537L;
+	long long ll = 12345678908642LL;//12'345'678'908'642LL
+
+	printf("char = %hhd, %d, %c\n", c, c, c);//여기서 %c는 c값을 문자로 바꿔서 출력해준다.(아스키 코드값에서의 65번)
+	printf("short = %hhd, %hd, %d\n", s, s, s);//short타입인데 char의 형식지정자(%hhd)를 지정해주면 overflow문제가 생긴다.
+	printf("unsigned int = %u, %d\n", ui, ui);//여기서도 %u는 unsinged int형식 지정자라 정상출력되지만 %d는 singed int형식 지정자라서 역시 overflow문제가 생긴다.
+	printf("long = %ld, %hd\n", l, l);//%ld는 long decimal 형식 지정자라 정상출력되지만, %hd는 long보다 작은 값의 크기를 다루기에 overflow문제가 생긴다.
+	printf("long long = %lld, %ld\n", ll, ll);//long long의 형식 지정자인 %lld는 정상출력되지만, %ld는 long형식 지정자라서 역시 overflow문제가 생긴다.
+
+	return 0;
+}
+*/
+
+int main()
+{
+	unsigned int decimal = 4294967295;//ui가 가질수있는 가장 큰값
+	unsigned int binary = 0b11111111111111111111111111111111;//0b를 넣으면 2진수를 나타냄(1은 32개임 = 32bit)
+	unsigned int oct = 037777777777;//8진수는 앞에 0을 붙임
+	unsigned int hex = 0xffffffff;//16진수는 x를 붙임(16진수는 실무에서 꽤 씀)
+
+	printf("%u\n", decimal);
+	printf("%u\n", binary);
+	printf("%u\n", oct);
+	printf("%u\n", hex);//출력해보면 전부다 10진수로 같은 수가 출력된다.
+
+	printf("%o %x %#o %#x %#X", decimal, decimal, decimal, decimal, decimal);
+	//여기서는 각각의 수들을 8진수나 16진수로 출력하는 코드이다.
+	//%o = octal, %x = hexadecimal, %# = 8진수 혹은 16진수앞에 붙는 0이나 0x를 붙여서 출력해준다, %#X = 대문자로 출력해준다.
+
+	return 0;
+}
---
+++ 섹션_3_code/chapter3_solution_sol/fixed_width_integers/FWI.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <stdint.h>		//고정 너비 자료형들을 담고있는 헤더파일
+#include <inttypes.h>	//고정 너비 자료형들의 형식 지정자를 담고있는 헤더파일
+						//inttypes.h만 include하면 stdint.h는 include안해도 됩니다.(inttypes.h에 포함되어 있음)
+
+int main()
+{
+	int i;
+	int32_t i32;		// 32 bit integer
+	int_least8_t i8;	// smallest 8 bit				//적어도 8bit를 가지고 있는 type
+	int_fast8_t f8;		// fastest minimum				//8bit중에서 가장 빠른 type
+	intmax_t imax;		// biggest signed integers		//signed int 중에서 가장 큰거
+	uintmax_t uimax;	// biggest unsigned integers	//unsigned int 중에서 가장 큰거
+
+	//위 변수들은 어떤 시스템으로 가던 해당 메모리 크기를 '고정'으로 할당받게된다.
+	//stdint.h 헤더파일 include해야 위 자료형들 사용가능
+
+	i32 = 1004;
+
+	printf("me32 = %d\n", i32);
+	printf("me32 = %" "d" "\n", i32);		//fomatting하는 부분을 쪼개서 작성한 코드
+	printf("me32 = %" PRId32 "\n", i32);	//PRI(print) d(decimal) 32(32bit)는 매크로이다.
+
+	return 0;
+}
