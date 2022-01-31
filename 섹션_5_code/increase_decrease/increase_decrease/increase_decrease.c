1ea5cc4b2c3e5245c76231a15ecd6ce1ad13bee3
Ha3To <lgsc0409ld@gmail.com>
2022-02-01 03:48:32 +0900

    증감연산자

---
+++ 섹션_5_code/increase_decrease/increase_decrease/increase_decrease.c
@@ -0,0 +1,63 @@
+//
+//  main.c
+//  increase_decrease
+//
+//  Created by 임태호 on 2022/02/01.
+//
+
+#include <stdio.h>
+
+int main() {
+    
+//    int a = 0;
+//    a++;// a = a + 1 or a += 1;
+//    printf("%d\n", a);
+//
+//    ++a;// a = a +1 or a += 1;
+//    printf("%d\n", a);
+//
+//    double b = 0;
+//    b++;
+//    printf("%f\n", b);
+//
+//    ++b;
+//    printf("%f\n", b);
+//
+//    int count = 0;
+//    while (count < 10)  // ++count or count++
+//    {
+//        printf("%d ", count++);   // ++count or count++
+//    }
+//
+//    int i = 1, j = 1;
+//    int i_post, pre_j;
+//
+//    i_post = i++;
+//    pre_j = ++j;
+//
+//    printf("%d %d\n", i, j);
+//    printf("%d %d\n", i_post, pre_j);
+//
+//    int i = 3;
+//    int l = 2 * --i;
+//    printf("%d %d\n", i, l);
+//
+//    i = 1;
+//    l = 2 * i--;
+//    printf("%d %d\n", i ,l);
+//
+//    int x, y, z;
+//    x = 3, y = 4;
+//    z = (x + y++) * 5;  // (x + y)++ or x+(y++) ?
+//    printf("%d %d %d\n", x, y, z);
+//
+//    int x = 1, y = 1, z;
+//    z = x * y++;    // (x) * (y++)
+//
+//    int n = 1;
+//    printf("%d %d\n", n, n * n++);
+//    int x = n / 2 + 5 * (1 + n++);
+//    int y = n++ + n++; // 이런식으로 증감연산자를 사용하지 말것!
+    
+    return 0;
+}
