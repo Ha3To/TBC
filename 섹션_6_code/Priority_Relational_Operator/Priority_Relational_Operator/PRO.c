89c542be2ae3092c4cde8909a953b51aba3a7bc8
Ha3To <lgsc0409ld@gmail.com>
2022-02-07 01:52:00 +0900

    lecture_6.7

---
+++ 섹션_6_code/Priority_Relational_Operator/Priority_Relational_Operator/PRO.c
@@ -0,0 +1,34 @@
+//
+//  main.c
+//  Priority_Relational_Operator
+//
+//  Created by 임태호 on 2022/02/06.
+//
+
+#include <stdio.h>
+
+int main() {
+    
+//    int x = 1, y = 2, z;
+    
+//    x > y + 2;
+//       👇
+//    x > (y + 2);
+
+    
+//    x = y > 2;
+//       👇
+//    x = (y > 2);
+
+    
+//    z = x > y;
+//       👇
+//    z = (x > y);
+
+    
+//    x != y == z;
+//       👇
+//    (x != y) == z;
+
+    return 0;
+}
